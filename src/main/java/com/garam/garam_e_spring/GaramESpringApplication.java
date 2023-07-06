1793ad4ddd430d881537d04a27c08d9817c09d85
이은섭 <dldmstjq99@gmail.com>
2023-07-06 15:21:57 +0900

    initial commit

---
+++ src/main/java/com/garam/garam_e_spring/GaramESpringApplication.java
@@ -0,0 +1,13 @@
+package com.garam.garam_e_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaramESpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaramESpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/garam/garam_e_spring/GaramESpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garam.garam_e_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaramESpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
